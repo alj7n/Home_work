ac791b75f10cc473708fdf45d5d6cfeb43301c7c
Abdullah <abdullah.alresaini@gmail.com>
2023-08-08 06:12:20 +0300

    HW day 1

---
+++ home work day 1/index.js
@@ -0,0 +1,11 @@
+// التمرين الأول
+
+let age =15;
+console.log(age);
+let name = "abdullah alresaini";
+console.log(name);
+// التمرين الثاني
+// const age2 = 15;
+// console.log(age2);
+// const age2 = 16;
+// console.log(age2);
