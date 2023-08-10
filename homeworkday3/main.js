a18ca47c7a9c82cab72a1fbcef6db2e30413e632
Abdullah <abdullah.alresaini@gmail.com>
2023-08-10 16:28:50 +0300

    done

---
+++ homeworkday3/main.js
@@ -0,0 +1,44 @@
+// التمرين الأول
+let num = 42;
+let numtostr = String(num)
+console.log(`${typeof numtostr} ${numtostr}`);
+
+let str = "25"
+let strtonum = Number(str)
+console.log(`${typeof strtonum} ${strtonum}`);
+
+// التمرين الثاني 
+
+let str1 = "10" + 3
+let str1tonum = Number(str1)
+console.log(`${typeof str1} ${str1}`);
+console.log(`${typeof str1tonum} ${str1tonum}`);
+ 
+
+// التمرين الثالث
+
+let str2 = "15.5" 
+let strfloat = parseFloat(str2)
+console.log(`${typeof strfloat} ${strfloat}`);
+console.log(`${typeof str2} ${str2}`);
+
+let hh = strfloat * 2
+console.log(`${typeof hh} ${hh}`);
+let mm = strfloat/3
+console.log(`${typeof mm} ${mm}`);
+
+
+// التمرين الرابع
+
+let str3 = "5"
+let num1 = 3
+let jj = str3 + num1
+console.log(jj);
+
+// التمرين الخامس
+let bool = true;
+let num2= 1;
+let gg= bool + num2
+console.log(gg);
+
+
