53e12f4a04bee57546ae16de822c52adbc73ae14
Abdullah <abdullah.alresaini@gmail.com>
2023-08-13 07:23:00 +0300

    done

---
+++ homeworkday4/main.js
@@ -0,0 +1,269 @@
+// abdullah ali alresaini
+
+// // boolean
+// let test1 = false;
+// let test2 = true;
+
+// console.log(test1);
+// console.log(test2);
+// console.log(typeof test1);
+
+// // symbol
+// let id = Symbol("id")
+
+
+// console.log(id);
+// console.log(typeof id);
+
+// // unll type
+
+// let test3= null;
+// console.log( test3);
+// console.log(typeof test3);
+
+// // undefined
+
+// let tset4 ;
+// console.log(tset4);
+// console.log(typeof tset4);
+
+// // Number
+
+// // integer 
+// // et 1l
+
+
+
+
+
+
+
+// // bigint
+
+// let big1 = 123456789n;
+// console.log(big1);
+// console.log(typeof big1);
+
+// let x0 = 20n-10n
+// console.log(x0);
+
+
+
+
+
+
+// // تحدي
+
+// let tuwaig = 2.20
+// console.log(tuwaig);
+// console.log(typeof tuwaig);
+// let tuwaig2 = 10n + 20n
+// console.log(tuwaig2);
+// console.log(typeof tuwaig2);
+
+// ///////////////////////////////////////////////////////
+
+// let test = "wep design";
+// console.log(test);
+// console.log(typeof test);
+// let test6 = "2008"
+// console.log(test6);
+// console.log(typeof test6);
+// let test7 = "abdullah 'ali' alresaini";
+// console.log(test7);
+// console.log(typeof test7);
+// let test8 = 'abdullah "ali" alresaini';
+// console.log(test8);
+// console.log(typeof test8);
+// let test9 = 'abdullah \"ali\" alresaini';
+// console.log(test9);
+// console.log(typeof test9);
+// let test10 = "abdullah \'ali\' alresaini";
+// console.log(test10);
+// console.log(typeof test10);
+
+// // string
+
+
+// let message = "test"-"test"
+// console.log(message);
+// console.log(typeof message);
+// let message1 = "30"-"10"
+// console.log(message1);
+// console.log(typeof message1);
+// let message2 = "30"+"10";
+// console.log(message2);
+// console.log(typeof message2);
+
+// // interpolation
+
+// let countrey ="Ryiadh"
+// let continent="Asia"
+// console.log(`${countrey} located in ${continent}`);
+
+// // String propreti
+
+// let src= "java script language";
+// // console.log(src.length);
+// // console.log(src.charAt(5));
+// // console.log(src.slice(0 ,11));
+// console.log(src.split("script "));
+
+// // /////////////
+// let src2= "java script language";
+
+// console.log(src2.length);
+// console.log('test'.length);
+
+// console.log(src2.charAt(0));
+// console.log('abc'.charAt(1));
+// // console.log(src2.slice(0 . 4));
+// console.log('test'.slice(1.3));
+
+// // console.log(src2.split(' '));
+
+// ////////////////////////////////////////////////////////////
+
+// let str = String();
+// let num = Number();
+// let bool = Boolean();
+
+// // str to str
+// const str1 ="text"
+// const strTostr = String(str1)
+// console.log(`${typeof str1}: ${str1}`);
+// console.log(`${typeof strTostr}: ${ strTostr}`);
+
+// // num to str
+// const num1 = 20;
+// const numTostr=String(num1)
+// console.log(`${typeof num1}: ${ num1}`);
+// console.log(`${typeof numTostr}: ${ numTostr}`);
+
+// // bool to str
+// const bool_1= true
+// const boolTostr = String(bool_1)
+// console.log(`${typeof bool_1}: ${bool_1 }`);
+// console.log(`${typeof boolTostr }: ${boolTostr }`);
+
+// // undefined to str 
+// const un=undefined;
+// const unTostr = String(un);
+// console.log(`${typeof un }: ${un}`);
+// console.log(`${typeof unTostr}: ${ unTostr}`);
+
+// // null to str
+// const null_1 = null;
+// const nullTostr =String(null_1)
+// console.log(`${typeof null_1}: ${ null_1}`);
+// console.log(`${typeof nullTostr}: ${nullTostr }`);
+
+// // BigInt to str 
+// const bigint_1 = 10n
+// const bigNTostr = String(bigint_1)
+// console.log(`${typeof bigint_1  }: ${ bigint_1}`);
+// console.log(`${typeof bigNTostr }: ${bigNTostr }`);
+
+// //////////////////////////////////////////////////////////////////////////////
+
+// // num to num 
+// const num_2= 30;
+// const numTonum= Number(num_2)
+// console.log(`${typeof num_2 }: ${ num_2}`);
+// console.log(`${typeof numTonum}: ${numTonum }`);
+
+// // str to num "text"
+// let str_2 = "text"
+// let strTonum = Number(str_2)
+// console.log(`${typeof str_2}: ${ str_2}`);
+// console.log(`${typeof strTonum}: ${ strTonum}`);
+
+// // str to num "11"
+// const str_3 = "11"
+// const strTonum2 = Number(str_2)
+// console.log(`${typeof str_3}: ${ str_3}`);
+// console.log(`${typeof strTonum2}: ${ strTonum2}`);
+
+// // Bigint to num 
+// const big2 = 20n;
+// const bignTostr=Number(big2)
+// console.log(`${typeof big2}: ${big2 }`);
+// console.log(`${typeof bignTostr}: ${ bignTostr}`);
+
+// // bool to num 
+// const bool_2=true
+// const bool_2Tonum=Number(bool_2)
+// console.log(`${typeof bool_2}: ${bool_2 }`);
+// console.log(`${typeof bool_2Tonum}: ${ bool_2Tonum}`);
+
+// // un to num
+// const nu2 = undefined
+// const nu2tonum= Number(nu2)
+// console.log(`${typeof nu2 }: ${nu2 }`);
+// console.log(`${typeof nu2tonum}: ${ nu2tonum}`);
+
+// // null to num 
+// const null_2 = null
+// const nullTonum = Number(null_2)
+// console.log(`${typeof null_2}: ${null_2 }`);
+// console.log(`${typeof nullTonum}: ${nullTonum }`);
+
+// //////////////////////////////////////////////////////////////////////////////
+
+// // str to bool
+// const str_4="wow"
+// const strtobool = Boolean(str_4)
+// console.log(`${typeof str_4}: ${ str_4}`);
+// console.log(`${typeof strtobool}: ${strtobool }`);
+
+// // num to bool
+// const num_3=80
+// const numtobool=Boolean(num_3)
+// console.log(`${typeof num_3}: ${num_3 }`);
+// console.log(`${typeof numtobool}: ${numtobool }`);
+// // bool to bool
+// const bool_3=true
+// const booltobool=Boolean(bool_3)
+// console.log(`${typeof bool_3 }: ${bool_3 }`);
+// console.log(`${typeof booltobool}: ${booltobool }`);
+
+// // bign to bool
+// const big4 = 38n
+// const bigntobool =Boolean(big4)
+// console.log(`${typeof big4}: ${big4 }`);
+// console.log(`${typeof bigntobool}: ${bigntobool }`);
+
+
+// // un to bool
+// const un3 = undefined
+// const untobool =Boolean(un3)
+// console.log(`${typeof un3}: ${un3 }`);
+// console.log(`${typeof untobool }: ${untobool }`);
+
+// // null to bool
+// const null_3=null
+// const nulltobool = Boolean(null_3)
+// console.log(`${typeof null_3}: ${ null_3}`);
+// console.log(`${typeof nulltobool}: ${nulltobool }`);
+
+// ////////////////////////////////////////////////////////////////////////////
+
+// // automatic conversion 
+// const kk = (22 + "1")
+// console.log(kk);
+// const jj = (23-1)
+// console.log(jj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
