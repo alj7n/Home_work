67fa3aa4df6a2ae7aa945df377d93304483de49a
Abdullah <abdullah.alresaini@gmail.com>
2023-08-14 16:08:32 +0300

    done

---
+++ homeworkday5/main.js
@@ -0,0 +1,10 @@
+let names = ["abdullah","ali","fahad"]
+console.log(names[2]);
+names.unshift("nora")
+console.log(names);
+names.push("saud")
+console.log(names);
+names.shift()
+console.log(names);
+names.pop()
+console.log(names);
